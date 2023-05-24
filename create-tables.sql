675079fa52f070d52ea720dc0735b7d0181c9f8d
Michael <rhift.michael@gmail.com>
2023-05-24 12:02:54 -0500

    Se agregaron las tablas Stations y Trains

--- create-tables.sql
+++ create-tables.sql
@@ -1,16 +1,19 @@
 -- alt + 96 para hacer las comillas invertidas ``
 USE metro_cdmx;
 
-CREATE TABLE `Lines` (
+CREATE TABLE `Trains` (
 
-    `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(10) NOT NULL,
-    `color` VARCHAR(15) NOT NULL,
+    `serial_number` VARCHAR(10) NOT NULL,
+    `line_id` BIGINT(20) UNSIGNED NOT NULL,
+    `type`  tinyint(4) NOT NULL,
+    `year` INT(4) NOT NULL,
     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     
-    PRIMARY KEY (id) 
+    PRIMARY KEY (serial_number),
+    CONSTRAINT `fk_Lines` FOREIGN KEY (`line_id`) REFERENCES `Lines` (id)
 
 )
 DEFAULT CHARSET = utf8mb4
 COLLATE = utf8mb4_unicode_ci; --Esta es la forma en la que la base de datos copara los caracteres.
+
